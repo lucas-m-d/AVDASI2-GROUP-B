21411b6a37272cfcfbe49f05ad604a4e05b4225c
lucas-m-d <68205310+lucas-m-d@users.noreply.github.com>
2024-09-23 15:37:36 +0100

    a bit of work

--- RCS-TMS/app.py
+++ RCS-TMS/app.py
@@ -4,12 +4,15 @@ from TMS.logging import *
 from datarefs import DataRef ## dataRef is a customisable list of data that is being tracked
 from dotenv import load_dotenv
 import os
+from flask_cors import CORS
+import time
 
 # set up with ports
-load_dotenv()
+load_dotenv(dotenv_path='../.env')
 PORT = os.getenv("RCSTMS_PORT")
 
 app = Flask(__name__)
+CORS(app)
 
 # for now
 dataList = [DataRef(time=12345678, flapLeft=0.5)]
@@ -17,6 +20,7 @@ dataList = [DataRef(time=12345678, flapLeft=0.5)]
 # set up app routings
 @app.route('/data', methods=['GET'])
 def httpGetData():
+    print(time.time())
     return getData(dataList)
 
 
