14bbe7edf365ed3578915601001f4b2231611ad0
lucas-m-d <68205310+lucas-m-d@users.noreply.github.com>
2024-11-02 13:21:23 +0000

    Cubepilot connecting work

---
+++ onboard/main.py
@@ -0,0 +1,16 @@
+from pymavlink import mavutil
+
+PORT = 'COM8'
+
+con = mavutil.mavlink_connection("tcp:localhost:5760")
+
+
+
+con.wait_heartbeat()
+print("Heartbeat from system (system %u component %u)" % (con.target_system, con.target_component))
+attitude = con.recv_match(type='ATTITUDE',blocking=True) ### this is how you get incoming messages from the cubepilot
+
+print(attitude)
+
+
+
